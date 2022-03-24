cb223b299caa9f5ffb022d82094a7b5c7f914372
alegw <alegw1@gmail.com>
2022-03-23 23:16:40 -0300

    subiendo el html de detalle de produto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express();
+const path = require ('path');
+app.use(express.static("public"));
+
+app.listen(5000, ()=> console.log("Esto fue exitoso"))
+
+
+app.get("/home", function (req,res){
+    res.sendFile(path.join(__dirname, "/views/index.html"))}
+)
+
+app.get("/detail", function (req,res){
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))}
+)
