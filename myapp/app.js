5fd70ae331fc431d54ee0839836bdd7a32084362
Alexis Garcia <alegw1@gmail.com>
2022-04-10 21:05:12 -0300

    agregando
    Agregando head y header de partials a archivos detail.
    agregue hojas de estilos al head
    redirigi a /detail1 foto pollera fuxia del index

--- myapp/app.js
+++ myapp/app.js
@@ -21,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/detail1', indexRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/detail1', function(req, res, next) {
+  res.render('productDetail1', { title: 'Express' });
+});
 module.exports = router;
