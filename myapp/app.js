9f3a4ae933e86b28e2b1ea37ec10cdc55b3e8c3f
Alexis Garcia <alegw1@gmail.com>
2022-04-10 21:09:51 -0300

    agrengando
    agregue redireccionamientos del resto de fotos del index.
    arregle el redireccionamiento del logo a /index
    ** ver forma de sacar enlaces app.use(...) del app.js y llevarlos al "usersRouter" porque solo me funcionan en "indexRouter"**

--- myapp/app.js
+++ myapp/app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/detail1', indexRouter)
+app.use('/detail2', indexRouter)
+app.use('/detail3', indexRouter)
+app.use('/detail4', indexRouter)
+app.use('/detail5', indexRouter)
+
 
 
 // catch 404 and forward to error handler
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -9,4 +9,17 @@ router.get('/', function(req, res, next) {
 router.get('/detail1', function(req, res, next) {
   res.render('productDetail1', { title: 'Express' });
 });
+router.get('/detail2', function(req, res, next) {
+  res.render('productDetail2', { title: 'Express' });
+});
+router.get('/detail3', function(req, res, next) {
+  res.render('productDetail3', { title: 'Express' });
+});
+router.get('/detail4', function(req, res, next) {
+  res.render('productDetail4', { title: 'Express' });
+});
+router.get('/detail5', function(req, res, next) {
+  res.render('productDetail5', { title: 'Express' });
+});
+
 module.exports = router;
