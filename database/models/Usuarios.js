4b969b517a2aade762177366d99c46b76272b876
andresFarias144 <99698095+andresFarias144@users.noreply.github.com>
2022-07-18 23:01:06 -0300

    Ajuste en modelo usuarios

--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -14,7 +14,7 @@ module.exports = function (sequelize, DataTypes){
             type: DataTypes.STRING
         },
         email: {
-            //type: DataTypes.STRING
+            type: DataTypes.STRING
         },
         phone: {
             type: DataTypes.INTEGER
