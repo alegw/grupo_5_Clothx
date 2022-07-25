5091710c83f99f8312cc6117c8e08bd4c27a68c1
andresFarias144 <99698095+andresFarias144@users.noreply.github.com>
2022-07-25 18:56:12 -0300

    Ajustes css

--- routes/main.js
+++ routes/main.js
@@ -18,17 +18,6 @@ router.get("/tyc", function (req, res, next) {
   res.render("tyc", { title: "Express" });
 });
 
-/* router.get("/login", function (req, res, next) {
-    res.render("login", { title: "Express" });
-}); ------> RE MUEVE A /USERS/LOGIN*/
-/* router.get("/register", function (req, res, next) {
-    res.render("register", { title: "Express" });
-});-------> SE MUEVE A /USERS/CREATE */
-
-/*   router.get("/reestablecer", function (req, res, next) {
-    res.render("reestablecer", { title: "Express" });
-  });-----> SE MUEVE A /USERS/REESTABLECER */
-
 
 
 module.exports = router;
